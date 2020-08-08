3d94209f906c9c7319d7a0a29857b974d519013d
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-08-07 20:14:23 -0700

    linear_search completed, MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,19 +1,17 @@
 def linear_search(arr, target):
-    pass
-    # for (el, idx) in arr:
-    #     print(el, '-', idx)
-    #     if el == target:
-    #         return idx
+    for idx, el in enumerate(arr):
+        if el == target:
+            return idx
 
-    # return -1   # not found
+    return -1   # not found
 
 # arr1 = [-2, 7, 3, -9, 5, 1, 0, 4, -6]
 # arr2 = []
 
-# print(linear_search(arr1, 6), '-1')
-# print(linear_search(arr1, -6), '8')
-# print(linear_search(arr1, 0), '6')
-# print(linear_search(arr2, 3), '-1')
+# print(linear_search(arr1, 6), '=-1?')
+# print(linear_search(arr1, -6), '=8?')
+# print(linear_search(arr1, 0), '=6?')
+# print(linear_search(arr2, 3), '=-1?')
 
 
 # Write an iterative implementation of Binary Search
